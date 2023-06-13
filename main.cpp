6daecf263e7e32d2a200e992b320d5daa4f7b41e
metasaturn2021 <132666006+metasaturn2021@users.noreply.github.com>
2023-06-14 06:04:23 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    char a[100]={"1234567"};
+    char b[100]={"abcde"};
+    
+    strcat(a,b);
+    cout<<a<<endl;
+    cout<<b<<endl;
+    /*
+    cout<<a<<endl;
+    for(int i=0;i<=10;i++){
+    	if (a[i]!='\0') cout<<a[i];
+    	else cout<<" ";
+	}*/
+    
+   
+    return 0;
+}
